0c1e36e5b911e0564497cffa975b88520e40e4b5
SourceCode <sourcecode042@gmail.com>
2022-10-27 16:09:52 +0100

    Clabs Commit

--- src/js/InjectCss.js
+++ src/js/InjectCss.js
@@ -2,7 +2,7 @@ const injectCSS = () => {
     let link = document.createElement("link");
     link.type = "text/css";
     link.rel = "stylesheet";
-    link.href = "./dist/styles.css";
+    link.href = "https://cdn.jsdelivr.net/gh/benjamin1234-ben/Clabs-AMADAO@master/dist/styles.css";
 
     let head = document.head;
     head.appendChild(link);
