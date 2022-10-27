a8c37da929713fcb352afdef5eac4253301955ca
SourceCode <sourcecode042@gmail.com>
2022-10-27 15:49:10 +0100

    Clabs Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 const $ = require("jquery");
+const Index = require("./html/index.html");
 
 const initJquery = () => {
     $(function() {
+        $("#clabs").html(Index);
+
         $("a.clabs-underline").on("click", () => {
             $("#download").hide();
         })
