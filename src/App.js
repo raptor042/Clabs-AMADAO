5efef000b90dcbc04f3d08aa16c602725cd79cd1
SourceCode <sourcecode042@gmail.com>
2022-10-27 16:12:28 +0100

    Clabs Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const $ = require("jquery");
 const Index = require("./html/index.html");
 
 const initJquery = () => {
+    console.log(Index);
     $(function() {
         $("#clabs").html(Index);
 
